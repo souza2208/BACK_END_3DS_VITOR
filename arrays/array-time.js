34b61ebdaafa992ed4e16ea027cd83f5558166ed
souza2208 <vitor.alexandre.souza@escola.pr.gov.br>
2026-03-04 11:48:55 +0000

    introduçao a arrays em js

---
+++ arrays/array-time.js
@@ -0,0 +1,8 @@
+const time = ["Jose", "ana"]
+time[5]
+time[6] = "victor"
+for (const jogador of time) {
+    console.log(jogador)
+}
+console.log(time)
+console.log(time[1])
---
+++ condicionais/if-login.js
@@ -0,0 +1,8 @@
+let login = "souza22"
+let senha = "22022008"
+
+if (login =="souza22") {
+ console.log   ("acesso certo" && senha == 22022008)
+}else{
+   console.log ("acesso negado") 
+}
