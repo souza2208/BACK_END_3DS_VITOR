6e31e16003804f6ccfce8bc78aa615e8881df337
souza2208 <vitor.alexandre.souza@escola.pr.gov.br>
2026-03-30 14:03:12 +0000

    funçoes

---
+++ funcoes/func-01.js
@@ -0,0 +1,10 @@
+function dizOla(){
+    console.log("Olá")
+}
+function olaPessoa(nome){
+    console.log("Olá, " + nome)
+}
+
+dizOla()
+olaPessoa("Luana")
+olaPessoa("Mano")
---
+++ funcoes/func-02.js
@@ -0,0 +1,34 @@
+//Crie 4 funções para operações (+, -, *, /)
+// Calcula e imprime resultado
+
+function subtracao(n1, n2){
+    const resultado = n1 - n2
+    console.log("Resultado da subtração: " + resultado)
+}
+
+subtracao(5, 2)
+subtracao(11, 7)
+
+function divisao(n1, n2){
+    const resultado = n1 / n2
+    console.log("Resultado da divisão: " + resultado)
+}
+
+divisao(4, 2)
+divisao(9, 3)
+
+function adicao(n1, n2){
+    const resultado = n1 + n2
+    console.log("Resultado da adição: " + resultado)
+}
+
+adicao(5, 2)
+adicao(7, 8)
+
+function multiplicacao(n1, n2){
+    const resultado = n1 * n2
+    console.log("Resultado da multiplicacao: " + resultado)
+}
+
+multiplicacao(5, 2)
+multiplicacao(7, 5)
