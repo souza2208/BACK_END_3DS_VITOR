815a5936e98103d5e58a1d2118d7ff61905b227e
souza2208 <vitor.alexandre.souza@escola.pr.gov.br>
2026-04-01 12:36:53 +0000

    fuc-03

---
+++ funcoes/fuc-03.js
@@ -0,0 +1,8 @@
+// enciado no seu caderno...
+function carregando () {
+        for (let i = 0; i <200; i++){
+            console.log("nao confie no igor...");
+        }
+}
+// chamar a função uma unica vez
+carregando()
